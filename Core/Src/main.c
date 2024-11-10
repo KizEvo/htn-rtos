62df9fb37f6f034ce2282ba8e3cf638c5ee6d2e0
Nguyễn Đức Phú <104358167+KizEvo@users.noreply.github.com>
2024-11-10 15:01:26 +0700

    main: add DMA read ADC and FLASH r/w operation (#1)
    * main: add DMA read ADC and FLASH r/w operation
    
    The ADC data is read periodically by TIM4 which has an interval
    of 500ms. When TIM4 trigger an interrupt (Counter Period reached)
    we start DMA1 to go collect the ADC data. The Task1 has higher
    interrupt priority than other Task, but it only process data when
    DMA finished collecting the ADC data so most of the time it's asleep.
    
    Task2 and Task3 do nothing for now, displaying LED2 and 3 to
    aid in debugging.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -22,7 +22,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include <assert.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -32,7 +32,10 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+#define ADC_Buffer_Size 1
 
+// x starts at 0 and end at 63
+#define FLASH_PAGE_X_OFFSET(x) (0x08000000UL + (0x400UL * x))
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -41,6 +44,12 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+ADC_HandleTypeDef hadc1;
+DMA_HandleTypeDef hdma_adc1;
+
+TIM_HandleTypeDef htim4;
+
+DMA_HandleTypeDef hdma_memtomem_dma1_channel2;
 osThreadId defaultTaskHandle;
 uint32_t defaultTaskBuffer[ 150 ];
 osStaticThreadDef_t defaultTaskControlBlock;
@@ -50,13 +59,21 @@ osStaticThreadDef_t myTask02ControlBlock;
 osThreadId myTask03Handle;
 uint32_t myTask03Buffer[ 150 ];
 osStaticThreadDef_t myTask03ControlBlock;
+osMessageQId ADC_QNameHandle;
+uint8_t ADC_QNameBuffer[ 16 * sizeof( uint32_t ) ];
+osStaticMessageQDef_t ADC_QNameControlBlock;
 /* USER CODE BEGIN PV */
-
+uint32_t ADC_Buffer;
+uint32_t FLASH_Data;
+extern FLASH_ProcessTypeDef pFlash;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
+static void MX_DMA_Init(void);
+static void MX_ADC1_Init(void);
+static void MX_TIM4_Init(void);
 void StartDefaultTask(void const * argument);
 void StartTask02(void const * argument);
 void StartTask03(void const * argument);
@@ -99,8 +116,11 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_ADC1_Init();
+  MX_TIM4_Init();
   /* USER CODE BEGIN 2 */
-
+	
   /* USER CODE END 2 */
 
   /* USER CODE BEGIN RTOS_MUTEX */
@@ -115,13 +135,18 @@ int main(void)
   /* start timers, add new ones, ... */
   /* USER CODE END RTOS_TIMERS */
 
+  /* Create the queue(s) */
+  /* definition and creation of ADC_QName */
+  osMessageQStaticDef(ADC_QName, 16, uint32_t, ADC_QNameBuffer, &ADC_QNameControlBlock);
+  ADC_QNameHandle = osMessageCreate(osMessageQ(ADC_QName), NULL);
+
   /* USER CODE BEGIN RTOS_QUEUES */
   /* add queues, ... */
   /* USER CODE END RTOS_QUEUES */
 
   /* Create the thread(s) */
   /* definition and creation of defaultTask */
-  osThreadStaticDef(defaultTask, StartDefaultTask, osPriorityNormal, 0, 150, defaultTaskBuffer, &defaultTaskControlBlock);
+  osThreadStaticDef(defaultTask, StartDefaultTask, osPriorityAboveNormal, 0, 150, defaultTaskBuffer, &defaultTaskControlBlock);
   defaultTaskHandle = osThreadCreate(osThread(defaultTask), NULL);
 
   /* definition and creation of myTask02 */
@@ -160,6 +185,7 @@ void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
@@ -188,6 +214,136 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_ADC;
+  PeriphClkInit.AdcClockSelection = RCC_ADCPCLK2_DIV6;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief ADC1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_ADC1_Init(void)
+{
+
+  /* USER CODE BEGIN ADC1_Init 0 */
+
+  /* USER CODE END ADC1_Init 0 */
+
+  ADC_ChannelConfTypeDef sConfig = {0};
+
+  /* USER CODE BEGIN ADC1_Init 1 */
+
+  /* USER CODE END ADC1_Init 1 */
+
+  /** Common config
+  */
+  hadc1.Instance = ADC1;
+  hadc1.Init.ScanConvMode = ADC_SCAN_DISABLE;
+  hadc1.Init.ContinuousConvMode = DISABLE;
+  hadc1.Init.DiscontinuousConvMode = DISABLE;
+  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc1.Init.NbrOfConversion = 1;
+  if (HAL_ADC_Init(&hadc1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Regular Channel
+  */
+  sConfig.Channel = ADC_CHANNEL_4;
+  sConfig.Rank = ADC_REGULAR_RANK_1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN ADC1_Init 2 */
+
+  /* USER CODE END ADC1_Init 2 */
+
+}
+
+/**
+  * @brief TIM4 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM4_Init(void)
+{
+
+  /* USER CODE BEGIN TIM4_Init 0 */
+
+  /* USER CODE END TIM4_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM4_Init 1 */
+
+  /* USER CODE END TIM4_Init 1 */
+  htim4.Instance = TIM4;
+  htim4.Init.Prescaler = 599;
+  htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim4.Init.Period = 50000;
+  htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim4.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
+  if (HAL_TIM_Base_Init(&htim4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim4, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim4, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM4_Init 2 */
+
+  /* USER CODE END TIM4_Init 2 */
+
+}
+
+/**
+  * Enable DMA controller clock
+  * Configure DMA for memory to memory transfers
+  *   hdma_memtomem_dma1_channel2
+  */
+static void MX_DMA_Init(void)
+{
+
+  /* DMA controller clock enable */
+  __HAL_RCC_DMA1_CLK_ENABLE();
+
+  /* Configure DMA request hdma_memtomem_dma1_channel2 on DMA1_Channel2 */
+  hdma_memtomem_dma1_channel2.Instance = DMA1_Channel2;
+  hdma_memtomem_dma1_channel2.Init.Direction = DMA_MEMORY_TO_MEMORY;
+  hdma_memtomem_dma1_channel2.Init.PeriphInc = DMA_PINC_ENABLE;
+  hdma_memtomem_dma1_channel2.Init.MemInc = DMA_MINC_ENABLE;
+  hdma_memtomem_dma1_channel2.Init.PeriphDataAlignment = DMA_PDATAALIGN_HALFWORD;
+  hdma_memtomem_dma1_channel2.Init.MemDataAlignment = DMA_MDATAALIGN_HALFWORD;
+  hdma_memtomem_dma1_channel2.Init.Mode = DMA_NORMAL;
+  hdma_memtomem_dma1_channel2.Init.Priority = DMA_PRIORITY_HIGH;
+  if (HAL_DMA_Init(&hdma_memtomem_dma1_channel2) != HAL_OK)
+  {
+    Error_Handler( );
+  }
+
+  /* DMA interrupt init */
+  /* DMA1_Channel1_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA1_Channel1_IRQn, 7, 0);
+  HAL_NVIC_EnableIRQ(DMA1_Channel1_IRQn);
+
 }
 
 /**
@@ -231,6 +387,59 @@ static void MX_GPIO_Init(void)
 
 /* USER CODE BEGIN 4 */
 
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
+{
+	if (hadc->Instance == ADC1) {
+			osMessagePut(ADC_QNameHandle, ADC_Buffer, 50);
+	}
+}
+
+static HAL_StatusTypeDef FLASH_DMA_Write(uint32_t Address, uint32_t Data)
+{
+	HAL_StatusTypeDef rc;
+	uint8_t nbiterations;
+	
+	/* Process Locked */
+	__HAL_LOCK(&pFlash);
+	// Wait for last operation to finish
+	rc = FLASH_WaitForLastOperation(FLASH_TIMEOUT_VALUE);
+	if (rc == HAL_OK) {
+		nbiterations = 2U;				
+		for (uint8_t index = 0U; index < nbiterations; index++) {
+			
+			/* Clean the error context */
+			pFlash.ErrorCode = HAL_FLASH_ERROR_NONE;
+			/* Proceed to program the new data */
+			SET_BIT(FLASH->CR, FLASH_CR_PG);
+			/* Write data to the flash address - currently not using DMA */
+			Address = Address + (2U * index);
+			Data = (uint16_t)(Data >> (16U * index));
+			/* Use CPU */
+			// *(__IO uint16_t*)Address = Data;
+			/* Use DMA */
+			HAL_DMA_Start(&hdma_memtomem_dma1_channel2, (uint32_t)&Data, Address, 1);
+			while (HAL_DMA_PollForTransfer(&hdma_memtomem_dma1_channel2, HAL_DMA_FULL_TRANSFER, 100) != HAL_OK);
+			/* End write to flash */
+			
+			/* Wait for last operation to be completed */
+			rc = FLASH_WaitForLastOperation(FLASH_TIMEOUT_VALUE);
+	
+			/* If the program operation is completed, disable the PG Bit */
+			CLEAR_BIT(FLASH->CR, FLASH_CR_PG);
+				
+			/* In case of error, stop writing to flash */
+			if (rc != HAL_OK) {
+				break;
+			}
+		}
+	}
+	
+	/* Process Unlocked */
+	__HAL_UNLOCK(&pFlash);
+	
+	return rc;
+}
+
 /* USER CODE END 4 */
 
 /* USER CODE BEGIN Header_StartDefaultTask */
@@ -244,10 +453,35 @@ void StartDefaultTask(void const * argument)
 {
   /* USER CODE BEGIN 5 */
   /* Infinite loop */
-  for(;;)
+	HAL_StatusTypeDef rc;
+	FLASH_EraseInitTypeDef pEraseInit;
+	uint32_t PageError;
+	
+	rc = HAL_TIM_Base_Start_IT(&htim4);
+	assert(rc == HAL_OK && "Cannot start TIM4 clock for ADC sampling application");
+  
+	pEraseInit.Banks = FLASH_BANK_1;
+	pEraseInit.NbPages = 1;
+	pEraseInit.PageAddress = FLASH_PAGE_X_OFFSET(50);
+	pEraseInit.TypeErase = FLASH_TYPEERASE_PAGES;
+	
+	for(;;)
   {
+		// event.value.v hold the actual message (eg. the ADC1 data)
+		osEvent event = osMessageGet(ADC_QNameHandle, osWaitForever);
 		HAL_GPIO_TogglePin(LED1_GPIO_Port, LED1_Pin);
-    osDelay(200);
+		// Unlock flash
+		HAL_FLASH_Unlock();
+		// Erase first
+		HAL_FLASHEx_Erase(&pEraseInit, &PageError);
+		// Write to flash
+		// HAL function to write to FLASH using CPU - HAL_FLASH_Program(FLASH_TYPEPROGRAM_WORD, FLASH_PAGE_X_OFFSET(50), event.value.v);
+		FLASH_DMA_Write(FLASH_PAGE_X_OFFSET(50), event.value.v);
+		// Lock flash
+		HAL_FLASH_Lock();
+		
+		// Read Flash
+		FLASH_Data = *(__IO uint32_t *)(FLASH_PAGE_X_OFFSET(50));
   }
   /* USER CODE END 5 */
 }
@@ -266,7 +500,7 @@ void StartTask02(void const * argument)
   for(;;)
   {
 		HAL_GPIO_TogglePin(LED2_GPIO_Port, LED2_Pin);
-    osDelay(200);
+		osDelay(200);
   }
   /* USER CODE END StartTask02 */
 }
@@ -284,8 +518,8 @@ void StartTask03(void const * argument)
   /* Infinite loop */
   for(;;)
   {
-		HAL_GPIO_WritePin(LED3_GPIO_Port, LED3_Pin, GPIO_PIN_SET);
-    osDelay(1000);
+		HAL_GPIO_TogglePin(LED3_GPIO_Port, LED3_Pin);
+		osDelay(200);
   }
   /* USER CODE END StartTask03 */
 }
@@ -307,7 +541,9 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
     HAL_IncTick();
   }
   /* USER CODE BEGIN Callback 1 */
-
+	if (htim->Instance == TIM4) {
+		HAL_ADC_Start_DMA(&hadc1, &ADC_Buffer, ADC_Buffer_Size);
+	}
   /* USER CODE END Callback 1 */
 }
 
--- Core/Src/stm32f1xx_it.c
+++ Core/Src/stm32f1xx_it.c
@@ -55,6 +55,8 @@
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
+extern DMA_HandleTypeDef hdma_adc1;
+extern TIM_HandleTypeDef htim4;
 extern TIM_HandleTypeDef htim2;
 
 /* USER CODE BEGIN EV */
@@ -160,6 +162,20 @@ void DebugMon_Handler(void)
 /******************************************************************************/
 
 /**
+  * @brief This function handles DMA1 channel1 global interrupt.
+  */
+void DMA1_Channel1_IRQHandler(void)
+{
+  /* USER CODE BEGIN DMA1_Channel1_IRQn 0 */
+
+  /* USER CODE END DMA1_Channel1_IRQn 0 */
+  HAL_DMA_IRQHandler(&hdma_adc1);
+  /* USER CODE BEGIN DMA1_Channel1_IRQn 1 */
+
+  /* USER CODE END DMA1_Channel1_IRQn 1 */
+}
+
+/**
   * @brief This function handles TIM2 global interrupt.
   */
 void TIM2_IRQHandler(void)
@@ -171,6 +187,20 @@ void TIM2_IRQHandler(void)
   /* USER CODE BEGIN TIM2_IRQn 1 */
 
   /* USER CODE END TIM2_IRQn 1 */
+}
+
+/**
+  * @brief This function handles TIM4 global interrupt.
+  */
+void TIM4_IRQHandler(void)
+{
+  /* USER CODE BEGIN TIM4_IRQn 0 */
+
+  /* USER CODE END TIM4_IRQn 0 */
+  HAL_TIM_IRQHandler(&htim4);
+  /* USER CODE BEGIN TIM4_IRQn 1 */
+
+  /* USER CODE END TIM4_IRQn 1 */
 }
 
 /* USER CODE BEGIN 1 */
